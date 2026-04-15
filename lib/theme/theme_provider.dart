ab7ec9931371edd15fe3dbbdd88cc4620e5550e7
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-15 22:26:00 +0600

    Dark mode implemented using riverpod and used sharedpreference to save the darkmode instance

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,30 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:habittracker/theme/dark_mode.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:habittracker/theme/light_mode.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-part 'theme_provider.g.dart';
-
-@riverpod
-class ThemeNotifier extends _$ThemeNotifier {
-  @override
-  ThemeData build() {
-    return lightMode;
-  }
-
-  //read data
-  Future<void> loadTheme() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final bool isDark = prefs.getBool('isDark') ?? false;
-    state = isDark ? darkMode : lightMode;
-  }
-
-  //change theme
-  void toggleTheme() async {
-    state = (state == darkMode) ? lightMode : darkMode;
-
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isDark', state == darkMode);
-  }
-}
+final themeProvider = StateProvider<ThemeData>((ref) => lightMode);
