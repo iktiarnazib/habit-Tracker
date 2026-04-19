3020306a83e91c8df90bb1a52f5ce462186fcc0f
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-19 23:26:09 +0600

    Improvements push from habit_repository

--- lib/repositories/habit_repository.dart
+++ lib/repositories/habit_repository.dart
@@ -10,6 +10,7 @@ class HabitNotifier extends AsyncNotifier<List<Habit>> {
     return db.select(db.habits).get();
   }
 
+  //add habit
   Future<void> addHabit(String name) async {
     final db = ref.read(databaseProvider);
     await db
@@ -18,12 +19,55 @@ class HabitNotifier extends AsyncNotifier<List<Habit>> {
     ref.invalidateSelf();
   }
 
-  Future<void> updateHabit(Habit habit) async {
+  // update completion status
+  Future<void> updateCompletionStatus(Habit habit, bool isCompleted) async {
     final db = ref.read(databaseProvider);
-    await db.update(db.habits).replace(habit);
+
+    final updatedDays = List<DateTime>.from(habit.completedDays);
+
+    if (isCompleted && !updatedDays.contains(DateTime.now())) {
+      // add today's date (date only, no time)
+      final today = DateTime(
+        DateTime.now().year,
+        DateTime.now().month,
+        DateTime.now().day,
+      );
+      updatedDays.add(today);
+    } else if (!isCompleted) {
+      // remove today if unchecked
+      updatedDays.removeWhere(
+        (d) =>
+            d.year == DateTime.now().year &&
+            d.month == DateTime.now().month &&
+            d.day == DateTime.now().day,
+      );
+    }
+
+    final updatedHabit = habit.copyWith(completedDays: updatedDays);
+    await db.update(db.habits).replace(updatedHabit);
     ref.invalidateSelf();
   }
 
+  // UPDATE - edit habit name
+  Future<void> updateHabitName(int id, String newName) async {
+    final db = ref.read(databaseProvider);
+
+    // find the specific habit
+    final habit = await (db.select(
+      db.habits,
+    )..where((t) => t.id.equals(id))).getSingleOrNull();
+
+    // update habit name
+    if (habit != null) {
+      final updatedHabit = habit.copyWith(name: newName);
+
+      // save updated habit back to the db
+      await db.update(db.habits).replace(updatedHabit);
+      ref.invalidateSelf();
+    }
+  }
+
+  //delete Habit
   Future<void> deleteHabit(int id) async {
     final db = ref.read(databaseProvider);
     await (db.delete(db.habits)..where((t) => t.id.equals(id))).go();
