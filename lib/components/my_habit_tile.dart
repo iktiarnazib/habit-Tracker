cd207522e2a4f36645dae31d3c2fbfb707378229
Iktiar Nazib <iktiarnazib@gmail.com>
2026-04-20 13:36:07 +0600

    Added my habit tile

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -13,25 +13,32 @@ class MyHabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: isCompleted
-            ? Colors.green
-            : Theme.of(context).colorScheme.secondary,
-      ),
-      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-      padding: EdgeInsets.all(15),
-      child: ListTile(
-        leading: Checkbox(
-          value: isCompleted,
-          onChanged: onChanged,
-          activeColor: Colors.white,
-          checkColor: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        if (onChanged != null) {
+          //toggle isCompleted
+          onChanged!(!isCompleted);
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: isCompleted
+              ? Colors.green
+              : Theme.of(context).colorScheme.secondary,
         ),
-        title: Text(
-          habit,
-          style: TextStyle(color: isCompleted ? Colors.white : Colors.black),
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+        padding: EdgeInsets.all(15),
+        child: ListTile(
+          leading: Checkbox(
+            value: isCompleted,
+            onChanged: onChanged,
+            activeColor: Colors.green,
+          ),
+          title: Text(
+            habit,
+            style: TextStyle(color: isCompleted ? Colors.white : Colors.black),
+          ),
         ),
       ),
     );
